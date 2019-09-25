6f057ef04fc7729e913ceb0459e24d2d765e78dd
selden.new@gmail.com <selden.new@gmail.com>
2019-09-25 18:47:44 +0500

    check short url

--- mysite/shortener/models.py
+++ mysite/shortener/models.py
@@ -1,22 +1,23 @@
 import string
+import requests
 from random import choices
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-import requests
-from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 
 
 class Url(models.Model):
     url_original = models.CharField(max_length=200)
-    url_short = models.CharField(default=None, max_length=20, unique=True)
+    url_short = models.CharField(default=None, max_length=6, unique=True)
     date_created = models.DateTimeField(default=timezone.now)
     clicks = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.url_original
@@ -29,6 +30,8 @@ class Url(models.Model):
     def generate_short_link(self):
         characters = string.digits + string.ascii_letters
         url_short = ''.join(choices(characters, k=6))
+        if Url.objects.filter(url_short=url_short).first():
+            return self.generate_short_link()
         return url_short
 
     def get_short_url(self):
@@ -39,7 +42,6 @@ class Url(models.Model):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:65.0) Gecko/20100101 Firefox/65.0'
         }
-
         try:
             requests.get(self.url_original, headers=headers)
         except(requests.RequestException, ValueError):
